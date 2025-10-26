4cbce6195cf5ae1dd445cde2508a19f4826c72eb
Sam Truong <144091973+SamTruongG@users.noreply.github.com>
2025-10-26 16:41:32 +1000

    Implement SVM model training with data preprocessing
    Added SVM model training and preprocessing steps.

--- ssc (4).py
+++ ssc (4).py
@@ -5,7 +5,16 @@ import numpy as np
 import pandas as pd
 import re
 
+# SVM model libraries
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+
+
 input_folder = "/Users/samtruong/Library/CloudStorage/OneDrive-GriffithUniversity/Desktop/3rd year/Stephen's Research/swallowing_segments"
+
 labelling_csv = "/Users/samtruong/Library/CloudStorage/OneDrive-GriffithUniversity/Desktop/3rd year/Stephen's Research/Swallowing1.csv"
 
 labels_df = pd.read_csv(labelling_csv)
@@ -81,8 +90,6 @@ for file in sorted_files:
         labels_list.append(label)
 
         
-        
-
     except Exception as e:
         print(f"Error processing {file}: {e}")
 
@@ -91,3 +98,21 @@ X = np.array(features_list)
 y = np.array(labels_list)
 
 print(f"Extracted features and labels for {len(X)} audio files.")
+
+
+#  TRAINING THE SVM MODEL #
+
+# splitting data into training and testing set
+# stratisfy helps to preserve the proportion when splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# scaling the feature
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# training the svm classifier 
+model = SVC(kernel='linear', C=1.0)
+model.fit(X_train, y_train)
+
+
